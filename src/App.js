f2d09adb0dccff9e84875a14728b466c2acc3c1a
Brandon Frausto <brandon.frausto18@gmail.com>
2022-04-19 21:10:21 -0500

    adjusted CSS properties to better fit mobile devices

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { BaseContainer } from "./components/baseContainer";
 
 function App() {
   return (
-    <div className="flex items-center justify-center flex-col bg-stone-800 text-white h-screen w-screen md:w-auto">
+    <div className="flex items-center flex-col bg-stone-800 text-white xs:h-screen w-screen md:w-auto">
       <h1 className="md:text-6xl text-4xl font-bold underline mt-4 mb-10 italic ">
         KILO-CALCULATOR
       </h1>
-      <div className="flex flex-col items-center bg-stone-600 rounded-lg h-4/6 md:h-full ">
+      <div className="flex flex-col items-center sm:bg-stone-600 rounded-lg xs:h-none md:h-4/6  ">
         <BaseContainer />
       </div>
       <p className="mt-5 text-lg">coded by Brandon Shin Frausto</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      xs: "200px",
+    },
     extend: {},
   },
   plugins: [require("tailwind-scrollbar")],
