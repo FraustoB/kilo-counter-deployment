fefa9170a2ed93eb4339d2312fb676eac2598160
Brandon Frausto <brandon.frausto18@gmail.com>
2022-04-19 21:56:02 -0500

    adjusted full screen DIV to allow for link at bottom of apge

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import { BaseContainer } from "./components/baseContainer";
 function App() {
   return (
     <div
-      className="flex items-center justify-center flex-col bg-stone-800 text-white h-screen 
+      className="flex items-center justify-center sm:justify-none flex-col bg-stone-800 text-white h-screen 
     w-screen "
     >
-      <h1 className="md:text-6xl text-4xl font-bold underline mt-4 mb-10 italic ">
+      <h1 className="md:text-6xl text-4xl font-bold underline mt-4 mb-5 italic ">
         KILO-CALCULATOR
       </h1>
-      <div className="flex flex-col items-center  bg-stone-600 rounded-lg  md:h-4/6  ">
+      <div className="flex flex-col items-center  bg-stone-600 rounded-lg  md:h-11/12  ">
         <BaseContainer />
       </div>
 
       <a
         href="https://frausto.dev/"
-        className="text-indigo-300 text-xl font-bold"
+        className="text-indigo-300 text-xl font-bold "
       >
         frausto.dev
       </a>
