a51b7775b1d660bc1704a4873063920bd0a97f82
FraustoB <brandon.frausto18@gmail.com>
2022-04-24 16:33:32 -0500

    deleted functions not being utilized, added comments to various functions to explain their functionality

--- src/features/plates/plateSlice.js
+++ src/features/plates/plateSlice.js
@@ -7,15 +7,20 @@ export const plateSlice = createSlice({
     value: [initStoreValue],
   },
   reducers: {
+    //add plate function adds a plate according to which button is pushed in plateAdder.js
     addPlate: (state, action) => {
       state.value.push(action.payload);
     },
+
+    // ** removes most recent plate that was added to the plate stack
     removePlate: (state) => {
       if (state.value.length >= 0) {
         state.value.pop();
       } else {
       }
     },
+
+    //completely resets the bar to an the intitial value
     resetPlate: (state) => {
       state.value = [initStoreValue];
     },
